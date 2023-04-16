881bf3a05208ce866682acfd080efdb3b8586edb
tato1599 <l21111240@cdjuarez.tecnm.mx>
2023-04-15 20:31:34 -0600

    Init

---
+++ ControlesPersonalizados/Buscador/Buscador.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ControlesPersonalizados.Buscador
+{
+    partial class Buscador
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.tbbuscar = new System.Windows.Forms.TextBox();
+            this.dgvCanciones = new System.Windows.Forms.DataGridView();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Artista = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Demo = new System.Windows.Forms.DataGridViewLinkColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCanciones)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnbuscar.ForeColor = System.Drawing.Color.Black;
+            this.btnbuscar.Location = new System.Drawing.Point(292, 26);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnbuscar.TabIndex = 0;
+            this.btnbuscar.Text = "Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = false;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // tbbuscar
+            // 
+            this.tbbuscar.Location = new System.Drawing.Point(19, 27);
+            this.tbbuscar.Name = "tbbuscar";
+            this.tbbuscar.Size = new System.Drawing.Size(267, 23);
+            this.tbbuscar.TabIndex = 1;
+            this.tbbuscar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbbuscar_KeyDown);
+            // 
+            // dgvCanciones
+            // 
+            this.dgvCanciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCanciones.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Nombre,
+            this.Artista,
+            this.Demo});
+            this.dgvCanciones.Location = new System.Drawing.Point(19, 55);
+            this.dgvCanciones.Name = "dgvCanciones";
+            this.dgvCanciones.RowTemplate.Height = 25;
+            this.dgvCanciones.Size = new System.Drawing.Size(348, 150);
+            this.dgvCanciones.TabIndex = 2;
+            this.dgvCanciones.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCanciones_CellContentClick);
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Artista
+            // 
+            this.Artista.HeaderText = "Artista";
+            this.Artista.Name = "Artista";
+            // 
+            // Demo
+            // 
+            this.Demo.HeaderText = "Demo";
+            this.Demo.Name = "Demo";
+            this.Demo.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Demo.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Buscador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvCanciones);
+            this.Controls.Add(this.tbbuscar);
+            this.Controls.Add(this.btnbuscar);
+            this.Name = "Buscador";
+            this.Size = new System.Drawing.Size(382, 219);
+            this.Load += new System.EventHandler(this.Buscador_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCanciones)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnbuscar;
+        private TextBox tbbuscar;
+        private DataGridView dgvCanciones;
+        private DataGridViewTextBoxColumn Nombre;
+        private DataGridViewTextBoxColumn Artista;
+        private DataGridViewLinkColumn Demo;
+    }
+}
---
+++ ControlesPersonalizados/Buscador/Buscador.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlesPersonalizados.Buscador
+{
+    public partial class Buscador : UserControl
+    {
+        public Buscador()
+        {
+            InitializeComponent();
+        }
+
+        private string clientID ="";
+        private string clientSecret="";
+
+        public string ClientID { get => clientID; set => clientID = value; }
+        public string ClientSecret { get => clientSecret; set => clientSecret = value; }
+
+        public string token()
+        {
+           
+
+            try
+            {
+                string credenciales = String.Format("{0}:{1}", clientID, clientSecret);
+                byte[] credencialesBytes = Encoding.UTF8.GetBytes(credenciales);
+                string credencialesBase64 = Convert.ToBase64String(credencialesBytes);
+
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credencialesBase64);
+
+                var parametros = new Dictionary<string, string>
+             {
+                 { "grant_type", "client_credentials" }
+            };
+
+
+
+                var content = new FormUrlEncodedContent(parametros);
+
+                var respuesta = client.PostAsync("https://accounts.spotify.com/api/token", content);
+                var json = respuesta.Result.Content.ReadAsStringAsync().Result;
+                JsonNode rootNode = JsonObject.Parse(json);
+
+                if (rootNode != null && rootNode["access_token"] != null)
+                {
+                    string token = rootNode["access_token"].ToString();
+                   
+                    return token;
+                }
+                else
+                {
+                
+                    MessageBox.Show("No se encontró el token de acceso en la respuesta JSON verifique el clientID y el clientSecret.");
+                    return null;
+                }
+
+               
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+
+        }
+
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            buscar();
+        }
+
+        private void buscar()
+        {
+            try
+            {
+                dgvCanciones.Rows.Clear();
+                HttpClient con = new HttpClient();
+
+                con.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token()); // Agregar token a la solicitud
+
+
+                var url = "https://api.spotify.com/v1/search?q=" + tbbuscar.Text + "&type=track&offset=0&limit=20";
+
+                var respuesta = con.GetAsync(url).Result;
+
+                string contenido = respuesta.Content.ReadAsStringAsync().Result;
+
+
+                JsonDocument doc = JsonDocument.Parse(contenido);
+                JsonElement root = doc.RootElement;
+
+                int musicaCant = root.GetProperty("tracks").GetProperty("items").GetArrayLength();
+
+
+
+                for (int i = 0; i < musicaCant; i++)
+                {
+                    //llenar data grid view
+                    dgvCanciones.Rows.Add(JsonObject.Parse(contenido)["tracks"]["items"][i]["name"].ToString(), JsonObject.Parse(contenido)["tracks"]["items"][i]["artists"][0]["name"].ToString(), JsonObject.Parse(contenido)["tracks"]["items"][i]["preview_url"].ToString());
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Warning");
+
+            }
+        }
+
+        private void Buscador_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvCanciones_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 2 && e.RowIndex >= 0)
+            {
+                string url = dgvCanciones.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+
+                if (Uri.TryCreate(url, UriKind.Absolute, out Uri uri) && (uri.Scheme == Uri.UriSchemeHttp || uri.Scheme == Uri.UriSchemeHttps))
+                {
+
+                    Process.Start(new ProcessStartInfo
+                    {
+                        FileName = "cmd",
+                        Arguments = $"/c start {url}",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    });
+                }
+            }
+        }
+
+        private void tbbuscar_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                buscar();
+            }
+        }
+    }
+}
